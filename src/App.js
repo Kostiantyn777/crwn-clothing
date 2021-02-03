db631122919dacefc167820ad6caf79ace5617fa
Kos <locoprodukten@gmail.com>
2021-02-03 21:02:18 +0100

    Added SignInAndSignUp Page and SignIn Component

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import "./App.css";
 import HomePage from "../src/pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "../src/components/header/header.component";
-
+import SignInAndSignUpPage from '../src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 function App() {
   return (
     <div>
@@ -14,6 +14,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
