701c4ce8dbe8a0aaa91c25b19a5d88ad1336c62f
Kos <locoprodukten@gmail.com>
2021-03-20 18:40:17 +0100

    Created CollectionOverview component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,22 +1,11 @@
 import React from "react";
 
-import { connect } from "react-redux";
-import { selectCollections } from "../../redux/shop/shop.selector.js";
+import CollectionOverview from "../../components/collection-overview/collection-overview.component";
 
-import { createStructuredSelector } from "reselect";
-
-import CollectionPreview from "../../components/collection-preview/collection-preview";
-
-const ShopPage = ({ collections }) => (
+const ShopPage = () => (
   <div className="shop-page">
-    {collections.map(({ id, ...otherCollectionProps }) => (
-      <CollectionPreview key={id} {...otherCollectionProps} />
-    ))}
+    <CollectionOverview />
   </div>
 );
 
-const mapStateToProps = createStructuredSelector({
-  collections: selectCollections,
-});
-
-export default connect(mapStateToProps)(ShopPage);
+export default ShopPage;
