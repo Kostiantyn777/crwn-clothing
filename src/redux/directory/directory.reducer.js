d97456ad75ad6ca61440bf3e2e973f982038adc9
Kos <locoprodukten@gmail.com>
2021-03-20 11:04:25 +0100

    Moved State from Directory component to Redux

---
+++ src/redux/directory/directory.reducer.js
@@ -0,0 +1,50 @@
+const INITIAL_STATE = {
+  sections: [
+    {
+      title: "hats",
+      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+      id: 1,
+      linkUrl: "shop/hats",
+    },
+    {
+      title: "jackets",
+      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+      id: 2,
+      linkUrl: "shop/jackets",
+    },
+    {
+      title: "sneakers",
+      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+      id: 3,
+      linkUrl: "shop/sneakers",
+    },
+    {
+      title: "womens",
+      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+      size: "large",
+      id: 4,
+      linkUrl: "shop/womens",
+    },
+    {
+      title: "mens",
+      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+      size: "large",
+      id: 5,
+      linkUrl: "shop/mens",
+    },
+  ],
+};
+
+const directoryReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default directoryReducer;
+
+//1.Write new reducer
+//2.Import it to root-reducer
+//3. Create selector
+//4. Pull this Redux state to my component
