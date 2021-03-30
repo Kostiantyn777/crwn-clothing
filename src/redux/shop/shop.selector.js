4f52f7e866bae698244a27cb1673f4bc194aabca
Kos <locoprodukten@gmail.com>
2021-03-30 10:16:18 +0200

    Data normalization + Collection Page

--- src/redux/shop/shop.selector.js
+++ src/redux/shop/shop.selector.js
@@ -1,13 +1,5 @@
 import { createSelector } from "reselect";
-import memoize from 'lodash.memoize';
-
-const COLLECTION_ID_MAP = {
-  hats: 1,
-  sneakers: 2,
-  jackets: 3,
-  womens: 4,
-  mens: 5,
-};
+import memoize from "lodash.memoize";
 
 const selectShop = (state) => state.shop;
 
@@ -17,8 +9,8 @@ export const selectCollections = createSelector(
 );
 
 export const selectCollection = memoize((collectionUrlParam) =>
-  createSelector([selectCollections], (collections) =>
-    collections.find(
-      (collection) => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
-    )
-  ));
+  createSelector(
+    [selectCollections],
+    (collections) => collections[collectionUrlParam]
+  )
+);
