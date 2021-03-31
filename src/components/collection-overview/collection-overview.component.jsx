896a0272062d8b9137b53858148f852b79962984
Kos <locoprodukten@gmail.com>
2021-03-31 10:03:31 +0200

    Added selectCollectionsForPreview selector

--- src/components/collection-overview/collection-overview.component.jsx
+++ src/components/collection-overview/collection-overview.component.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import "./collections-overview.styles.scss";
 
 import { connect } from "react-redux";
-import { selectCollections } from "../../redux/shop/shop.selector";
+import { selectCollectionsForPreview } from "../../redux/shop/shop.selector";
 import { createStructuredSelector } from "reselect";
 
 import CollectionPreview from "../../components/collection-preview/collection-preview.component";
@@ -17,7 +17,7 @@ const CollectionOverview = ({ collections }) => (
 );
 
 const mapStateToProps = createStructuredSelector({
-  collections: selectCollections,
+  collections: selectCollectionsForPreview,
 });
 
 export default connect(mapStateToProps)(CollectionOverview);
