4c5a1529fa6c08dc21df722aecc0c8b3f9088ac6
Kos <locoprodukten@gmail.com>
2021-04-01 11:17:32 +0200

    Stripe Integration

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  //Converting price to cents
+  const priceForStripe = price * 100;
+
+  const publishableKey =
+    "pk_test_51Ib07nGRwBTeDzvdupgfGcJLKVNxsEi6eKNx8KrtLzrsFWulO9T7HKKtWHUzjV01WIn5kL8pWXS9EainTkt591Jv00iS06VTxq";
+
+  const onToken = (token) => {
+    console.log(token);
+    alert("Payment Successful");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="CRWN CLOTHING"
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
