75ab8eae23f818600a6c2f4f91aa8b414b664082
Kos <locoprodukten@gmail.com>
2021-04-09 10:56:26 +0200

    Improved responsiveness

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { setCurrentUser } from "./redux/user/user.actions";
 import { selectCurrentUser } from "./redux/user/user.selector";
 import { createStructuredSelector } from "reselect";
 
-import "./App.css";
+import { GlobalStyles } from "./global.styles";
 
 import HomePage from "../src/pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
@@ -41,6 +41,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
+        <GlobalStyles />
         <Header />
         <Switch>
           <Route exact path="/" component={HomePage} />
